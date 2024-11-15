f7f64298224e5fca52814bef1609e563703517fe
The <nkt19910@gmail.com>
2024-11-16 02:34:01 +0700

    feat: finish

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import Programs from './Components/Programs/Programs'
@@ -8,7 +8,11 @@ import Campus from './Components/Campus/Campus'
 import Testimonials from './Components/Testimonials/Testimonials'
 import Contact from './Components/Contact/Contact'
 import Footer from './Components/Footer/Footer'
+import VideoPlayer from './Components/VideoPlayer/VideoPlayer'
 const App = () => {
+
+  const [playState,setPlayState]= useState(false);
+
   return (
     <div>
       <Navbar/>
@@ -16,7 +20,7 @@ const App = () => {
         <div className='container'>
           <Title subTitle='Our program' title='What we Offer' />
           <Programs/>
-          <About/>
+          <About setPlaystate={setPlayState}/>
           <Title subTitle='Gallery' title='Campus photos' />
           <Campus/>
           <Title subTitle='TESTIMONIALS' title='What Student Says' />
@@ -25,6 +29,7 @@ const App = () => {
           <Contact/>
           <Footer/>
         </div>
+        <VideoPlayer playState={playState} setPlayState={setPlayState}/>
     </div>
   )
 }
